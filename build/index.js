c4f357bcc8c2c75e483a07f9ac55a1fc51865ce3
Steven <steven.blasi@creativware.net>
2020-06-06 09:15:16 -0400

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testMethod = void 0;
+function testMethod(param) {
+    return "Hello " + param;
+}
+exports.testMethod = testMethod;
+//# sourceMappingURL=index.js.map
